91081def1489cd1d5de41b11b2f4d67800d3c20a
stephane888 <kksasteph888@gmail.com>
2022-03-15 19:01:06 +0100

    MAJ

--- src/Utility.php
+++ src/Utility.php
@@ -59,4 +59,30 @@ class Utility {
     return $er;
   }
   
+  /**
+   * Traite les message d'erreus lié à \Error.
+   * PHP 5
+   *
+   * @param Exception $e
+   * @param Number $nbr_trace
+   *        pour limiter le nombre d'erreur à afficher.
+   * @return boolean[]|NULL[]
+   */
+  public static function errorAll(\Throwable $e, int $nbr_trace = 3) {
+    $er = [
+      'message' => $e->getMessage(),
+      'code' => $e->getCode(),
+      'file' => $e->getFile(),
+      'line' => $e->getLine(),
+      'previous' => $e->getPrevious(),
+      'trace' => array_slice($e->getTrace(), 0, 3),
+      'PHP_execution_error' => true
+    ];
+    if ($nbr_trace)
+      $er['trace'] = array_slice($e->getTrace(), 0, $nbr_trace);
+    else
+      $er['trace'] = $e->getTrace();
+    return $er;
+  }
+  
 }
