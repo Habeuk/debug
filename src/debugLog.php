d7fd18f3051b369818222ef1d099bca34240bb21
kouwa stephane <kksasteph888@gmail.com>
2023-02-27 10:27:24 +0100

    Maj

--- src/debugLog.php
+++ src/debugLog.php
@@ -7,14 +7,14 @@ use kint\Utils;
 use Kint\Renderer\RichRenderer;
 
 class debugLog {
-  
+
   /**
    * le path doit etre relatif.
    *
    * @var string
    */
   public static $path = null;
-  
+
   /**
    * default value 3
    *
@@ -28,7 +28,7 @@ class debugLog {
   public static $masterFileName = null;
   public static $themeName = null;
   public static $debug = true;
-  
+
   /**
    * Debug php files or save value on file.
    *
@@ -48,7 +48,7 @@ class debugLog {
         $filename = self::$masterFileName;
       }
     }
-    
+
     if ($auto || self::$auto) {
       $filename = $filename . rand(1, 999);
     }
@@ -62,7 +62,7 @@ class debugLog {
       $path_of_module = '/' . $path_of_module;
       $path_of_module = str_replace("//", "/", $path_of_module);
     }
-    
+
     if (!file_exists($path_of_module)) {
       if (self::$debug)
         echo ('dossier en cour de creation dans :' . $path_of_module);
@@ -72,7 +72,7 @@ class debugLog {
       catch (\Exception $e) {
         return;
       }
-      
+
       if ($test_create) {
         chmod($path_of_module, 0755);
         if (self::$debug)
@@ -84,13 +84,13 @@ class debugLog {
         return;
       }
     }
-    
+
     $filename = $path_of_module . '/' . $filename;
-    
+
     if (!empty(self::$use)) {
       $use = self::$use;
     }
-    
+
     // Traitement des données.
     if ($use == 'file') {
       if (is_array($data) || is_object($data)) {
@@ -113,7 +113,7 @@ class debugLog {
       }
       $logs = PHP_EOL . PHP_EOL . 'Date : ' . date("d/m/Y  H:i:s") . '' . PHP_EOL;
       $result = $logs . $result;
-      
+
       if (self::$PositionAddLogAfter) {
         $monfichier = fopen($filename, "a+");
       }
@@ -127,7 +127,7 @@ class debugLog {
         fputs($monfichier, $result);
         fclose($monfichier);
       }
-      
+
       return true;
     }
     //
@@ -143,7 +143,7 @@ class debugLog {
       // DebugWbu::VarDumperBug($data);
       $result = ob_get_clean();
     }
-    
+
     $monfichier = fopen($filename, 'w+');
     if ($monfichier) {
       if ($result !== Null && $monfichier) {
@@ -155,116 +155,7 @@ class debugLog {
       echo "file not writable : " . $filename . '<br>';
     }
   }
-  
-  /**
-   *
-   * @deprecated use logger
-   * @param mixed $data
-   * @param string $filename
-   * @param boolean $auto
-   * @param string $use
-   * @param string $path_of_module
-   * @param boolean $usePath
-   * @return boolean
-   */
-  public static function logs($data, $filename = null, $auto = FALSE, $use = 'kint', $path_of_module = 'logs', $usePath = false) {
-    if (!$filename) {
-      $filename = 'debug';
-      if (self::$masterFileName) {
-        $filename = self::$masterFileName;
-      }
-    }
-    
-    if ($auto || self::$auto) {
-      $filename = $filename . rand(1, 999);
-    }
-    if (!empty(self::$path)) {
-      $path_of_module = self::$path;
-    }
-    if (defined('FULLROOT_WBU') && !self::$forcePath && !$usePath) {
-      $path_of_module = FULLROOT_WBU . '/' . $path_of_module;
-    }
-    elseif (!$usePath) {
-      $path_of_module = '/' . $path_of_module;
-    }
-    
-    if (!file_exists($path_of_module . '/files-log')) {
-      if (self::$debug) {
-        try {
-          echo ('dossier en cour de creation dans :' . $path_of_module);
-          if (mkdir($path_of_module . '/files-log', 0755, TRUE)) {
-            chmod($path_of_module . '/files-log', 0755);
-            if (self::$debug)
-              echo (' Dossier OK ');
-          }
-        }
-        catch (\Exception $e) {
-          if (self::$debug)
-            echo (' Echec creation dossier ');
-        }
-      }
-    }
-    $filename = $path_of_module . '/files-log/' . $filename;
-    
-    if (!empty(self::$use)) {
-      $use = self::$use;
-    }
-    
-    // Traitement des données.
-    if ($use == 'file') {
-      $result = $data;
-    } //
-    elseif ($use == 'json') {
-      $filename = $filename . '.json';
-      $result = $data;
-    } //
-    elseif ($use == 'log') {
-      if (is_array($data) || is_object($data)) {
-        ob_start();
-        print_r($data);
-        $result = ob_get_clean();
-      }
-      else {
-        $result = $data;
-      }
-      $logs = PHP_EOL . PHP_EOL . 'Date : ' . date("d/m/Y  H:i:s") . '' . PHP_EOL;
-      $result = $logs . $result;
-      
-      try {
-        if (self::$PositionAddLogAfter) {
-          $monfichier = fopen($filename, "a+");
-        }
-        else {
-          if (file_exists($filename)) {
-            $result .= file_get_contents($filename);
-          }
-          $monfichier = fopen($filename, "w");
-        }
-        if ($result !== Null && $monfichier) {
-          fputs($monfichier, $result);
-          fclose($monfichier);
-        }
-      }
-      catch (\Exception $e) {
-        //
-      }
-      return true;
-    }
-    else {
-      $filename = $filename . '.html';
-      ob_start();
-      DebugWbu::kint_bug($data, self::$max_depth);
-      // DebugWbu::VarDumperBug($data);
-      $result = ob_get_clean();
-    }
-    
-    $monfichier = fopen($filename, 'w+');
-    if ($result !== NULL && $monfichier) {
-      fputs($monfichier, $result);
-      fclose($monfichier);
-    }
-  }
-  
+
   public static function kintDebugDrupal($data, $filename = 'debug', $auto = false, $path_of_module = null) {
     if (empty($path_of_module)) {
       if (self::$themeName) {
@@ -276,9 +167,9 @@ class debugLog {
       }
     }
     $use = 'kint';
-    self::logs($data, $filename, $auto, $use, $path_of_module);
+    self::logger($data, $filename, $auto, $use, $path_of_module);
   }
-  
+
   /**
    * Methode de debugage inspirer de symfony.
    */
@@ -298,7 +189,7 @@ class debugLog {
     $usePath = false;
     self::logger($data, $filename, $auto, $use, $path_of_module, $usePath);
   }
-  
+
   public static function DebugDrupal($data, $filename = 'debug', $auto = false, $path_of_module = null, $use = 'log') {
     if (empty($path_of_module)) {
       if (self::$themeName) {
@@ -309,9 +200,9 @@ class debugLog {
         $path_of_module = DRUPAL_ROOT . '/' . drupal_get_path('theme', $defaultThemeName);
       }
     }
-    self::logs($data, $filename, $auto, $use, $path_of_module);
+    self::logger($data, $filename, $auto, $use, $path_of_module);
   }
-  
+
   public static function SaveLogsDrupal($data, $filename = 'debug', $path_of_module = null) {
     if (empty($path_of_module)) {
       if (self::$themeName) {
@@ -324,22 +215,22 @@ class debugLog {
     }
     $use = 'log';
     $auto = false;
-    self::logs($data, $filename, $auto, $use, $path_of_module);
+    self::logger($data, $filename, $auto, $use, $path_of_module);
   }
-  
+
   public static function saveLogs($data, $filename = 'debug', $path_of_module = 'logs') {
     $use = 'log';
     $auto = false;
-    self::logs($data, $filename, $auto, $use, $path_of_module);
+    self::logger($data, $filename, $auto, $use, $path_of_module);
   }
-  
+
   public static function saveJson(array $data, $filename = 'debug', $path_of_module = 'logs') {
     $use = 'json';
     $auto = false;
     $data = \json_encode($data);
-    self::logs($data, $filename, $auto, $use, $path_of_module);
+    self::logger($data, $filename, $auto, $use, $path_of_module);
   }
-  
+
   public static function savexml($data, $filename = null, $auto = false) {
     if (!$filename) {
       $filename = 'debug';
@@ -361,11 +252,11 @@ class debugLog {
           echo (' Echec creation dossier');
       }
     }
-    
+
     $filename = $path_of_module . '/files-xml/' . $filename . '.xml';
     $monfichier = fopen($filename, 'w+');
     fputs($monfichier, $data);
     fclose($monfichier);
   }
-  
+
 }
