17e2590c2826a1bffc2351ebabf91d0725c3cd5d
kouwa stephane <kksasteph888@gmail.com>
2023-08-07 08:29:12 +0200

    Maj

--- src/ExceptionDebug.php
+++ src/ExceptionDebug.php
@@ -9,52 +9,62 @@ use LogicException;
  * debocage par defaut.
  *
  * @author stephane
- *
+ *        
  */
 class ExceptionDebug extends LogicException implements \Throwable {
   protected $dbg;
-
+  
   /**
    * L'ide est de sauvegarder les informations de debug ($dbg) dans le fichier
    * de log par defaut.
    *
    * @param string $message
    * @param int $code
-   *        On s'inpire de des codes d'erreurs de monolog. @see
-   *        https://github.com/Seldaek/monolog/blob/2.x/doc/01-usage.md#log-levels
+   *        la veleur doit etre choisie avec soing au risque d'avoir de mauvaise
+   *        supprise dans le message.$this
+   *        419-420 Unassigned
+   *        432-450 Unassigned
+   *        452-499 Unassigned
+   * @see https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
+   *
    * @param mixed $previous
    * @param mixed $dbg
    */
-  function __construct($message = null, $dbg = null, $code = 100, $previous = null) {
+  function __construct($message = null, $dbg = null, $code = 432, $previous = null) {
     $this->dbg = $dbg;
-    $message = $message . ' || @debug ' . json_encode($dbg);
+    if ($dbg)
+      $message = $message . ' || @debug ' . json_encode($dbg);
     parent::__construct($message, $code, $previous);
   }
-
+  
   function getContentToDebug() {
     return $this->dbg;
   }
-
+  
   function setContentToDebug($dbg) {
     $this->dbg = $dbg;
   }
-
+  
   public function setErrors(array $errors) {
     $this->setContentToDebug($errors);
   }
-
+  
   public function getErrors() {
     return $this->dbg;
   }
-
+  
   function getErrorCode() {
-    if (empty($this->getCode()))
-      return 431;
     return $this->getCode();
   }
-
-  static public function exception($message, $dbg) {
+  
+  /**
+   *
+   * @param string $message
+   * @param mixed $dbg
+   * @return \Stephane888\Debug\ExceptionDebug
+   */
+  static public function exception($message, $dbg = null) {
     return new self($message, $dbg);
   }
-
+  
 }
