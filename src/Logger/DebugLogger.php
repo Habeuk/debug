43a0b796c09fdb4887024753aae12105f22a52f2
Justin-Jordan <justi2017@outlook.fr>
2023-10-27 09:01:53 +0100

    Squashed commit of the following:
    commit fc4f6c893ca16b98623cab8e3c37d990f101d4d6
    Author: Justin-Jordan <justi2017@outlook.fr>
    Date:   Fri Oct 13 18:00:07 2023 +0100
    
        add monolog to send logs via Email
    
    commit 495e1db8199e813dec8918b052dc46c219be70ed
    Author: Justin-Jordan <justi2017@outlook.fr>
    Date:   Thu Apr 13 12:10:10 2023 +0100
    
        update
    
    commit 03951a4d9d0e6fc3da150eae230a7bca8bade240
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Sun Sep 24 08:45:33 2023 +0200
    
        Maj
    
    commit b72e74853a193a15b5bb4603266cefd60e2ea162
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Mon Aug 7 08:30:58 2023 +0200
    
        Maj
    
    commit 17e2590c2826a1bffc2351ebabf91d0725c3cd5d
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Mon Aug 7 08:29:12 2023 +0200
    
        Maj
    
    commit a322e2e5dd94804921726b9a3652ee1ff3cf0842
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Mon Apr 17 18:38:55 2023 +0200
    
        l'ajout du contenu de variable au message d'erreur est ok
    
    commit e59e15935d991bb07a909ce3c2b7a0a76f60e061
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Mon Apr 17 18:31:17 2023 +0200
    
        L'ide est de sauvegarder les informations de debug () dans le fichier
           * de log par defaut.
    
    commit af7d9b155f23ce375c2979234470a6ad3e8854bd
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Mon Apr 17 16:48:18 2023 +0200
    
        Maj
    
    commit 4838bb2d64d42f83010cd3dba0e88f04d9bc97b0
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Sun Apr 9 18:21:36 2023 +0200
    
        Maj
    
    commit c60b5386076d416348f636ba8a3fe0f1faebbcf3
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Sun Mar 19 18:14:45 2023 +0100
    
        Maj
    
    commit d7fd18f3051b369818222ef1d099bca34240bb21
    Author: kouwa stephane <kksasteph888@gmail.com>
    Date:   Mon Feb 27 10:27:24 2023 +0100
    
        Maj
    
    commit b5514d72be489e81099a05ffdb24506914dfd33f
    Author: Justin-Jordan <justi2017@outlook.fr>
    Date:   Mon Oct 16 11:59:13 2023 +0100
    
        fix small issue with variable names
    
    commit 11918fc37a216d02e526b97a3e0e755d56c1ed32
    Author: Justin-Jordan <justi2017@outlook.fr>
    Date:   Fri Oct 13 18:00:07 2023 +0100
    
        add monolog to send logs via Email
    
    commit f5aec362db70333dd92ea3e7b45537a5fb402132
    Author: Justin-Jordan <justi2017@outlook.fr>
    Date:   Thu Apr 13 12:10:10 2023 +0100
    
        update

---
+++ src/Logger/DebugLogger.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace Stephane888\Debug\Logger;
+
+use Exception;
+use Monolog\Formatter\HtmlFormatter;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Processor\IntrospectionProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
+use Monolog\Processor\WebProcessor;
+use PHPMailer\PHPMailer\PHPMailer;
+use Psr\Log\LogLevel;
+use Stephane888\Debug\Logger\PHPMailerHandler;
+
+class DebugLogger implements DebugLoggerInterface
+{
+    private $logger;
+
+    /**
+     * The values for PHPMailer to send email via SMTP
+     * [
+     * 'host' => '', // mail.example.com
+     * 'sender' => '', // john@example.com
+     * 'sender_name' => '', // Loggin Habeuk
+     * 'port' => '' // 587
+     * 'user_name => '' // johndoe
+     * 'password => '' // *******
+     * 'recipients => [] // [ 'john@example.com', 'doe@example.com' ]
+     * ]
+     *
+     * @var array
+     */
+    public static $smtpSettings = [
+        'host' => '',
+        'sender' => '',
+        'sender_name' => '',
+        'port' => '',
+        'user_name' => '',
+        'password' => '',
+        'recipients' => [],
+    ];
+
+    /**
+     * The log directory path // /var/www/my-app/logs
+     */
+    public static $logDir = '';
+
+    
+    public function __construct(string $channel)
+    {
+        $this->logger = new Logger($channel);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function info($message, array $contenData = [], $fileName = "info"): void
+    {
+        $this->logger->pushHandler(new RotatingFileHandler($this->getFileDir() . "/$fileName.log", 7));
+        $this->logger->info($message, $contenData);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function notice($message, array $contenData = [], $fileName = "notice"): void
+    {
+        $this->logger->pushHandler(new RotatingFileHandler($this->getFileDir() . "/$fileName.log", 7));
+        $this->logger->notice($message, $contenData);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function debug($message, array $contenData = [], $fileName = "debug"): void
+    {
+        $this->logger->pushHandler(new RotatingFileHandler($this->getFileDir() . "/$fileName.log", 7));
+        $this->logger->notice($message, $contenData);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function warning($message, array $contenData = [], $fileName = "warning"): void
+    {
+        if (!key_exists("RECIPIENTS", $_ENV)) {
+            throw new Exception("RECIPIENTS env variable must be define", 1);
+        }
+
+        if (!key_exists("SENDER", $_ENV)) {
+            throw new Exception("SENDER env variable must be define", 1);
+        }
+
+        $smtpSettings = DebugLogger::$smtpSettings;
+
+        $phpmailer = new PHPMailer();
+        $phpmailer->isSMTP();
+        $phpmailer->Host = $smtpSettings['host'];
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Port = $smtpSettings['port'];
+        $phpmailer->Username = $smtpSettings['user_name'];
+        $phpmailer->Password = $smtpSettings['password'];
+
+        $phpmailer->setFrom($smtpSettings['sender'], 'Logging Server');
+        foreach ($smtpSettings['recipients'] as $recipient) {
+            $phpmailer->addAddress($recipient);
+        }
+
+        $this->logger->pushProcessor(new IntrospectionProcessor);
+        $this->logger->pushProcessor(new MemoryUsageProcessor);
+        $this->logger->pushProcessor(new WebProcessor);
+
+        $handler = new PHPMailerHandler($phpmailer);
+        $handler->setFormatter(new HtmlFormatter);
+
+        $this->logger->pushHandler($handler);
+        $this->logger->pushHandler(new StreamHandler($this->getFileDir() . "$fileName.log", LogLevel::ERROR));
+        $this->logger->warning($message, $contenData);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function error($message, array $contenData = [], $fileName = "error"): void
+    {
+        if (!key_exists("RECIPIENTS", $_ENV)) {
+            throw new Exception("RECIPIENTS env variable must be define", 1);
+        }
+
+        if (!key_exists("SENDER", $_ENV)) {
+            throw new Exception("SENDER env variable must be define", 1);
+        }
+
+        $smtpSettings = DebugLogger::$smtpSettings;
+
+        $phpmailer = new PHPMailer();
+        $phpmailer->isSMTP();
+        $phpmailer->Host = $smtpSettings['host'];
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Port = $smtpSettings['port'];
+        $phpmailer->Username = $smtpSettings['user_name'];
+        $phpmailer->Password = $smtpSettings['password'];
+
+        $phpmailer->setFrom($smtpSettings['sender'], 'Logging Server');
+        foreach ($smtpSettings['recipients'] as $recipient) {
+            $phpmailer->addAddress($recipient);
+        }
+
+        $this->logger->pushProcessor(new IntrospectionProcessor);
+        $this->logger->pushProcessor(new MemoryUsageProcessor);
+        $this->logger->pushProcessor(new WebProcessor);
+
+        $handler = new PHPMailerHandler($phpmailer);
+        $handler->setFormatter(new HtmlFormatter);
+
+        $this->logger->pushHandler($handler);
+        $this->logger->pushHandler(new StreamHandler($this->getFileDir() . "$fileName.log", LogLevel::ERROR));
+        $this->logger->error($message, $contenData);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function critical($message, array $contenData = [], $fileName = "critical"): void
+    {
+        if (!key_exists("RECIPIENTS", $_ENV)) {
+            throw new Exception("RECIPIENTS env variable must be define", 1);
+        }
+
+        if (!key_exists("SENDER", $_ENV)) {
+            throw new Exception("SENDER env variable must be define", 1);
+        }
+
+        $smtpSettings = DebugLogger::$smtpSettings;
+
+        $phpmailer = new PHPMailer();
+        $phpmailer->isSMTP();
+        $phpmailer->Host = $smtpSettings['host'];
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Port = $smtpSettings['port'];
+        $phpmailer->Username = $smtpSettings['user_name'];
+        $phpmailer->Password = $smtpSettings['password'];
+
+        $phpmailer->setFrom($smtpSettings['sender'], 'Logging Server');
+        foreach ($smtpSettings['recipients'] as $recipient) {
+            $phpmailer->addAddress($recipient);
+        }
+
+        $this->logger->pushProcessor(new IntrospectionProcessor);
+        $this->logger->pushProcessor(new MemoryUsageProcessor);
+        $this->logger->pushProcessor(new WebProcessor);
+
+        $handler = new PHPMailerHandler($phpmailer);
+        $handler->setFormatter(new HtmlFormatter);
+
+        $this->logger->pushHandler($handler);
+        $this->logger->pushHandler(new StreamHandler($this->getFileDir() . "$fileName.log", LogLevel::ERROR));
+        $this->logger->critical($message, $contenData);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function alert($message, array $contenData = [], $fileName = "alert"): void
+    {
+        if (!key_exists("RECIPIENTS", $_ENV)) {
+            throw new Exception("RECIPIENTS env variable must be define", 1);
+        }
+
+        if (!key_exists("SENDER", $_ENV)) {
+            throw new Exception("SENDER env variable must be define", 1);
+        }
+
+        $smtpSettings = DebugLogger::$smtpSettings;
+
+        $phpmailer = new PHPMailer();
+        $phpmailer->isSMTP();
+        $phpmailer->Host = $smtpSettings['host'];
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Port = $smtpSettings['port'];
+        $phpmailer->Username = $smtpSettings['user_name'];
+        $phpmailer->Password = $smtpSettings['password'];
+
+        $phpmailer->setFrom($smtpSettings['sender'], 'Logging Server');
+        foreach ($smtpSettings['recipients'] as $recipient) {
+            $phpmailer->addAddress($recipient);
+        }
+
+        $this->logger->pushProcessor(new IntrospectionProcessor);
+        $this->logger->pushProcessor(new MemoryUsageProcessor);
+        $this->logger->pushProcessor(new WebProcessor);
+
+        $handler = new PHPMailerHandler($phpmailer);
+        $handler->setFormatter(new HtmlFormatter);
+
+        $this->logger->pushHandler($handler);
+        $this->logger->pushHandler(new StreamHandler($this->getFileDir() . "$fileName.log", LogLevel::ERROR));
+        $this->logger->alert($message, $contenData);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function emergency($message, array $contenData = [], $fileName = "emergency"): void
+    {
+        if (!key_exists("RECIPIENTS", $_ENV)) {
+            throw new Exception("RECIPIENTS env variable must be define", 1);
+        }
+
+        if (!key_exists("SENDER", $_ENV)) {
+            throw new Exception("SENDER env variable must be define", 1);
+        }
+
+        $smtpSettings = DebugLogger::$smtpSettings;
+
+        $phpmailer = new PHPMailer();
+        $phpmailer->isSMTP();
+        $phpmailer->Host = $smtpSettings['host'];
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Port = $smtpSettings['port'];
+        $phpmailer->Username = $smtpSettings['user_name'];
+        $phpmailer->Password = $smtpSettings['password'];
+
+        $phpmailer->setFrom($smtpSettings['sender'], 'Logging Server');
+        foreach ($smtpSettings['recipients'] as $recipient) {
+            $phpmailer->addAddress($recipient);
+        }
+
+        $this->logger->pushProcessor(new IntrospectionProcessor);
+        $this->logger->pushProcessor(new MemoryUsageProcessor);
+        $this->logger->pushProcessor(new WebProcessor);
+
+        $handler = new PHPMailerHandler($phpmailer);
+        $handler->setFormatter(new HtmlFormatter);
+
+        $this->logger->pushHandler($handler);
+        $this->logger->pushHandler(new StreamHandler($this->getFileDir() . "$fileName.log", LogLevel::ERROR));
+        $this->logger->emergency($message, $contenData);
+    }
+
+    /**
+     * Return the log directory
+     */
+    protected function getFileDir(): string
+    {
+        if (DebugLogger::$logDir != '') {
+            return DebugLogger::$logDir;
+        }
+        return dirname(__FILE__, 3) . '/logs';
+    }
+}
