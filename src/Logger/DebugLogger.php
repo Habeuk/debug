dc6cb80dbbabfcf5dadbd62cb882d7cccb49405c
kouwa stephane <kksasteph888@gmail.com>
2023-11-23 12:06:18 +0100

    check if debug var is string for method alert

--- src/Logger/DebugLogger.php
+++ src/Logger/DebugLogger.php
@@ -151,10 +151,8 @@ class DebugLogger implements DebugLoggerInterface {
     $handler->setFormatter(new HtmlFormatter());
     $this->logger->pushHandler($handler);
     $this->logger->pushHandler(new StreamHandler($this->getFileDir() . "$fileName.log", LogLevel::ERROR));
-    if (!is_array($messageError)) {
-      $messageError = [
-        $messageError
-      ];
+    if (!is_string($messageError)) {
+      $messageError = (string) $messageError;
     }
     $this->logger->alert($messageError, $contenData);
   }
